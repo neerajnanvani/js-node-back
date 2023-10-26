560b412b013451895a5fbe5cc68f22b84f906fd9
neerajnanvani <neerajnanvaniwork@gmail.com>
2023-10-26 10:57:32 +0530

    init repo and created first response

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+    res.send("This is home");
+})
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
